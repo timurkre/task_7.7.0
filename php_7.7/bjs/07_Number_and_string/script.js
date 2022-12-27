53a1cae1d02d16e5a6f5ea5a659707086eb64ac4
Timur Nabiullin <timurkre@gmail.com>
2022-12-28 01:47:39 +0600

    first commit

---
+++ php_7.7/bjs/07_Number_and_string/script.js
@@ -0,0 +1,41 @@
+let lastOperand = 0;
+let operation = null;
+
+const inputWindow = document.getElementById('inputWindow');
+
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += "1";
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation ='sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation ='def';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum'){
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def'){
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+document.getElementById('btn_clr').addEventListener('click', function () {
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = '';
+})
